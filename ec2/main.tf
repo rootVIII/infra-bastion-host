cbfd1912972912f4c54ec18c1113220fef320886
rootVIII <colleyloyejames@gmail.com>
2023-12-18 20:39:04 -0500

    use resource instead of module for EC2

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,13 +1,21 @@
 
-module "ec2_instance" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "5.5.0"
-  for_each               = toset(["1", "2"])
-  name                   = "instance-${each.key}"
+resource "aws_instance" "bastion_host" {
   ami                    = var.ami
   instance_type          = "t2.nano"
-  key_name               = "ec2-user"
   vpc_security_group_ids = [var.default_security_group_id]
   subnet_id              = var.public_subnet_id
-  tags                   = var.project_tags
+  tags = {
+    Name = "BastionHost"
+  }
+}
+
+
+resource "aws_instance" "private_host" {
+  ami                    = var.ami
+  instance_type          = "t2.nano"
+  vpc_security_group_ids = [var.default_security_group_id]
+  subnet_id              = var.private_subnet_id
+  tags = {
+    Name = "PrivateHost"
+  }
 }
