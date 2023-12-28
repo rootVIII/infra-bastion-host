ad0cea1bedf82236e96d0b358235d949c62f23bf
rootVIII <colleyloyejames@gmail.com>
2023-12-28 18:08:31 -0500

    revert ssh key creation

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,17 +1,4 @@
 
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "key_pair" {
-  key_name   = "infra-key"
-  public_key = tls_private_key.private_key.public_key_openssh
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.private_key.private_key_pem}' > infra-key.pem"
-  }
-}
-
 resource "aws_instance" "bastion_host" {
   ami                    = var.ami
   instance_type          = "t2.nano"
