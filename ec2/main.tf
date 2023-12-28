a2fe37d3895dfc5f62d817962c39c0d0398f938c
rootVIII <colleyloyejames@gmail.com>
2023-12-28 17:49:03 -0500

    attempt to create ssh keys

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,23 @@
 
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "infra-key"
+  public_key = tls_private_key.private_key.public_key_openssh
+  provisioner "local-exec" {
+    command = "echo '${tls_private_key.private_key.private_key_pem}' > infra-key.pem"
+  }
+}
+
 resource "aws_instance" "bastion_host" {
   ami                    = var.ami
   instance_type          = "t2.nano"
   vpc_security_group_ids = [var.default_security_group_id]
   subnet_id              = var.public_subnet_id
+
   tags = {
     Name = "BastionHost"
   }
