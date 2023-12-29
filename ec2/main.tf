40aa4a27e3dda36c1d3d137db7192b90f5eb0313
rootVIII <colleyloyejames@gmail.com>
2023-12-28 19:05:37 -0500

    install SSH public key onto Bastion host

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,15 @@
 
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "infra-key"
+  public_key = file("infra-key.pub")
+}
+
 resource "aws_instance" "bastion_host" {
   ami                    = var.ami
   instance_type          = "t2.nano"
   vpc_security_group_ids = [var.default_security_group_id]
   subnet_id              = var.public_subnet_id
+  key_name               = aws_key_pair.ssh_key.key_name
 
   tags = {
     Name = "BastionHost"
