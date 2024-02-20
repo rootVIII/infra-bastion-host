f8d7f722e67f87651611c8c5b51f8b185c5b798e
rootVIII <colleyloyejames@gmail.com>
2024-02-20 00:37:24 -0500

    revise security groups for bastion and private instances

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,26 +1,30 @@
+resource "aws_security_group" "bastion_sg" {
+  description = "Security group for bastion host"
 
-resource "aws_security_group" "server_sg" {
-  vpc_id = var.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  # Outbound rule to allow access to web on any port
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_instance_sg" {
+  description = "Security group for private EC2 instance"
+
+  # Allow SSH from the bastion host security group
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion_sg.id]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -37,7 +41,7 @@ resource "aws_key_pair" "ssh_key" {
 resource "aws_instance" "bastion_host" {
   ami                         = var.ami
   instance_type               = "t2.nano"
-  vpc_security_group_ids      = [aws_security_group.server_sg.id]
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   subnet_id                   = var.public_subnet_id
   key_name                    = aws_key_pair.ssh_key.key_name
   associate_public_ip_address = "true"
@@ -50,9 +54,9 @@ resource "aws_instance" "bastion_host" {
 resource "aws_instance" "private_host" {
   ami                    = var.ami
   instance_type          = "t2.nano"
-  vpc_security_group_ids = [aws_security_group.server_sg.id]
+  vpc_security_group_ids = [aws_security_group.private_instance_sg.id]
   subnet_id              = var.private_subnet_id
-
+  key_name                    = aws_key_pair.ssh_key.key_name
   tags = {
     Name = "PrivateHost"
   }
