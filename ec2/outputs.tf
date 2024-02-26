c10079ba7e1dc1cbf3a79e96a5ccb9111fa2837d
rootVIII <colleyloyejames@gmail.com>
2024-02-25 19:50:37 -0500

    add public and private IP addresses to ec2 module outputs and root module outputs

--- ec2/outputs.tf
+++ ec2/outputs.tf
@@ -1,1 +1,10 @@
 
+output "public_ip" {
+  description = "IP address of bastion host EC2 instance"
+  value       = aws_instance.bastion_host.public_ip
+}
+
+output "private_IP" {
+  description = "IP address of private EC2 instance"
+  value       = aws_instance.private_host.private_ip
+}
--- environments/prd/outputs.tf
+++ environments/prd/outputs.tf
@@ -8,3 +8,13 @@ output "region" {
   description = "region"
   value       = var.region
 }
+
+output "bastion_host_ip" {
+  description = "Public IP address of public bastion instance/host"
+  value       = module.ec2.public_ip
+}
+
+output "private_host_ip" {
+  description = "Private IP address of private EC2 instance/host"
+  value       = module.ec2.private_IP
+}
